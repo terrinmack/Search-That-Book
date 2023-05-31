117946a83dff26d16998b37a6409c2f63edc9711
Your Name <terrimack15@gmail.com>
2023-05-31 13:41:15 -0700

    working through auth.js folder. reupdating after remaking repo

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -25,12 +25,15 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
+      return req;
     }
 
     // send to next endpoint
-    next();
+    // next();
+    return req;
   },
+  
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
